51aa102dff2c1b834b9eeaac318d449d52711faa
swastik0019 <venturesharsh@gmail.com>
2025-01-15 16:38:41 +0530

    User and Captain Login and Register integration Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import UserLogin from './pages/userLogin'
+import Start from './pages/Start'
+import UserLogin from './pages/UserLogin'
 import UserSignup from './pages/UserSignUp'
 import CaptainSignup from './pages/CaptainSignup'
 import CaptainLogin from './pages/CaptainLogin'
+import Home from './pages/Home'
+import UserProtectWrapper from './pages/UserProtectWrapper'
+import UserLogout from './pages/UserLogout'
+import CaptainProtectWrapper from './pages/CaptainProtectWrapper'
+import CaptainHome from './pages/CaptainHome'
+import CaptainLogout from './pages/CaptainLogout'
 
 const App = () => {
   return (
@@ -12,11 +18,36 @@ const App = () => {
     <div>
       
       <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Start />} />
           <Route path="/login" element={<UserLogin />} />
           <Route path="/signup" element={<UserSignup />} />
           <Route path="/captain-signup" element={<CaptainSignup />} />
           <Route path="/captain-login" element={<CaptainLogin />} />
+          <Route path="/home" element={
+            <UserProtectWrapper>
+              <Home />
+            </UserProtectWrapper>
+          } />
+
+          <Route path='/user/logout'
+            element={<UserProtectWrapper>
+              <UserLogout />
+            </UserProtectWrapper>
+          } />
+
+          <Route path='/captain-home' element={
+            <CaptainProtectWrapper>
+              <CaptainHome />
+            </CaptainProtectWrapper>
+
+          } />
+
+          <Route path='/captain/logout' element={
+            <CaptainProtectWrapper>
+              <CaptainLogout />
+            </CaptainProtectWrapper>
+          } />
+
       </Routes>
 
     </div>
--- frontend/src/pages/CaptainLogin.jsx
+++ frontend/src/pages/CaptainLogin.jsx
@@ -1,73 +1,87 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { CaptainDataContext } from '../context/CaptainContext'
 
-const CaptainLogin = () => {
+const Captainlogin = () => {
 
-  const [email,setEmail] = useState('');
-  const [password,setPassword] = useState('');
-  const [captainData,setCaptainData] = useState({});
-  
-  const submitHandler = (e) => {
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+
+  const { captain, setCaptain } = React.useContext(CaptainDataContext)
+  const navigate = useNavigate()
+
+
+
+  const submitHandler = async (e) => {
     e.preventDefault();
-      
-    setCaptainData({
-      email,
+    const captain = {
+      email: email,
       password
-    })
-      
-    setEmail('');
-    setPassword('');
-  }
+    }
 
+    const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/captains/login`, captain)
+
+    if (response.status === 200) {
+      const data = response.data
+
+      setCaptain(data.captain)
+      localStorage.setItem('captain-token', data.token)
+      navigate('/captain-home')
+
+    }
+
+    setEmail('')
+    setPassword('')
+  }
   return (
     <div className='p-7 h-screen flex flex-col justify-between'>
-
       <div>
-        
-        <img className='w-20 mb-3' src="https://www.svgrepo.com/show/505031/uber-driver.svg" alt="" />
+      <img className='w-16 mb-10' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYQy-OIkA6In0fTvVwZADPmFFibjmszu2A0g&s" alt="" />
 
-        <form action="" onSubmit={(e) => submitHandler(e)}>
-
+        <form onSubmit={(e) => {
+          submitHandler(e)
+        }}>
           <h3 className='text-lg font-medium mb-2'>What's your email</h3>
-          <input 
-            type="email" 
+          <input
+            required
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
-            required 
-            placeholder='email@example.com' 
+            onChange={(e) => {
+              setEmail(e.target.value)
+            }}
+            className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
+            type="email"
+            placeholder='email@example.com'
           />
 
           <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
-          <input 
-            type="password" 
-            required 
+
+          <input
+            className='bg-[#eeeeee] mb-7 rounded-lg px-4 py-2 border w-full text-lg placeholder:text-base'
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder='Password' 
-            className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base'
+            onChange={(e) => {
+              setPassword(e.target.value)
+            }}
+            required type="password"
+            placeholder='password'
           />
 
           <button
-            className='bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg placeholder:text-base'
+            className='bg-[#111] text-white font-semibold mb-3 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
           >Login</button>
 
-
         </form>
-          <p className='text-center'>Join a fleet ? <Link to={"/captain-signup"} className='text-blue-600 '>Register as a Captain</Link></p>
+        <p className='text-center'>Join a fleet? <Link to='/captain-signup' className='text-blue-600'>Register as a Captain</Link></p>
       </div>
-
-
       <div>
-        <Link to={"/login"}
-            className='bg-[#d5622d] flex justify-center items-center text-white font-semibold mb-7 rounded px-4 py-2 w-full text-lg placeholder:text-base'
-          >
-            Sign in as User
-          </Link>
+        <Link
+          to='/login'
+          className='bg-[#d5622d] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base'
+        >Sign in as User</Link>
       </div>
-
     </div>
   )
 }
 
-export default CaptainLogin
+export default Captainlogin
